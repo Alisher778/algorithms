058d5d9a524d69df482e2d6b4ba3e20096d2907d
Alisher778 <alisher.musurmonov89@gmail.com>
2018-11-30 21:51:02 +0500

    finished sock merchant algorithm

--- 01.warm-up-challenges/01.sock_merchant/index.js
+++ 01.warm-up-challenges/01.sock_merchant/index.js
@@ -1,5 +1,5 @@
 function sockMerchant(n, arr) {
-    var arrOfSocks = {};
+   let arrOfSocks = {};
     for (let i = 0; i < n; i++) {
         if (arrOfSocks[arr[i]]) {
             arrOfSocks[arr[i]]++;
@@ -7,8 +7,20 @@ function sockMerchant(n, arr) {
             arrOfSocks[arr[i]] = 1
         }
     }
-    console.log(arrOfSocks);
+    
+   let result = 0;
+    for(let x in arrOfSocks) {
+        if(arrOfSocks[x] % 2 == 0) {
+            result += arrOfSocks[x] / 2;
+        } else {
+            if((arrOfSocks[x] - 1) > 1 && (arrOfSocks[x] - 1) / 2 > 0) {
+                result += (arrOfSocks[x] - 1) / 2;
+            }
+        }
+    }
+    
+    return result;
 
 }
 
-sockMerchant(9, [10, 20, 20, 10, 10, 30, 50, 10, 20]);
+sockMerchant(12, [10, 20, 20, 10, 10, 30, 30, 50, 10, 50, 10, 20]);
