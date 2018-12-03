3ce49c003d0b6bb6919ba14f47c3f0025be2d740
Alisher778 <alisher.musurmonov89@gmail.com>
2018-12-03 21:15:57 +0500

    finished algorithms

---
+++ 00.challenges_series/03.diagonal-difference/main.js
@@ -0,0 +1,11 @@
+function diagonalDifference(arr) {
+	let leftDiagonal = 0;
+	let rightDiagonal = 0;
+
+	for(let i=0, j=arr.length - 1; i<arr.length; i++, j--) {
+		leftDiagonal += arr[i][i];
+		rightDiagonal += arr[i][j];
+	}
+	console.log(rightDiagonal - leftDiagonal)
+}
+diagonalDifference([[1,2,3], [4,5,6], [7,8,4]]);
