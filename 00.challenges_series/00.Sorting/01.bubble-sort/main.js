233d02fa675a6931a6acec96a9f67d6c59e1f9c2
Alisher778 <alisher.musurmonov89@gmail.com>
2018-12-06 22:21:23 +0500

    finished bubblesort algorithm

--- 00.challenges_series/00.Sorting/01.bubble-sort/main.js
+++ 00.challenges_series/00.Sorting/01.bubble-sort/main.js
@@ -1,7 +1,17 @@
 console.log('Bubble sort');
 
 function bubbleSort(arr) {
-
+    var myArr = [].concat(arr);
+    for(var i=0; i<myArr.length; i++) {
+        for(var j=0; j<myArr.length -1; j++) {
+            if(myArr[j]>myArr[j+1]) {
+                var currentValue = myArr[j];
+                myArr[j] = myArr[j+1];
+                myArr[j+1] = currentValue;
+            }
+        }
+    }
+    console.log(myArr);
 }
 
 bubbleSort([3,1,2,5,7,4]);
