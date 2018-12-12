2eb8e5b5f940cc688e36f1c6323b05e5a8a36204
Alisher778 <alisher.musurmonov89@gmail.com>
2018-12-12 23:21:14 +0500

    finished selection sort problem

--- 02.InterviewPrep/01.Sorting/05.selection-sort/main.js
+++ 02.InterviewPrep/01.Sorting/05.selection-sort/main.js
@@ -2,7 +2,13 @@ function selectionSort(arr) {
     let newArr = [].concat(arr);
     let result = [];
 
-    for(let i=0;)
+    for(let i=0;i<arr.length; i++) {
+        const min = Math.min.apply(null, newArr);
+        result.push(min);
+        newArr.splice(newArr.indexOf(min),1);
+    }
+
+    console.log(result);
 
 }
 
